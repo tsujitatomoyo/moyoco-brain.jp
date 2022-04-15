e60620de4e91c33103e3dde419357365386c9997
辻田智代 <mshxtom@gmail.com>
2022-04-16 07:18:38 +0900

    no message

--- wp-content/themes/muso/pages/works/banner/page-banner_list.php
+++ wp-content/themes/muso/pages/works/banner/page-banner_list.php
@@ -2,7 +2,7 @@
 	
 
 
-<div class="works_bn_wrap works_random_wrap">
+<div class="works_bn_wrap <?php /*?>works_random_wrap<?php */?>">
 
   <section id="bn_44">
       <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_44.jpg" alt=""></div>
@@ -34,10 +34,77 @@
       </ul>
     </section>
 	
+	
+	
+  <section id="bn_17">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_17.jpg" alt=""></div>
+      <h3 class="works_bn_h"><span>福岡ベルエポック美容専門学校様</span><br>
+        出張オープンキャンパス 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				女性　17〜18才<br>
+				○ペルソナ<br>
+				美容とファッションに興味のある女子高生</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">出張ということがわかるように各地の地名などを記載しました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">1:30〜2:00</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+	
+	
+	
+  <section id="bn_18">
+      <div class="works_bn_main_img">
+		  <ul class="col__wrap--margin">
+          <li class="col--pc-8 col--sp-12"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_18.jpg" alt=""></li>
+          <li class="col--pc-4 col--sp-12"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_18_2.jpg" alt=""></li>
+		  </ul>
+		  </div>
+      <h3 class="works_bn_h"><span>福岡ベルエポック美容専門学校様</span><br>
+        パンフレットの資料請求 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">パンフレットの資料請求</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				女性　17〜18才<br>
+				○ペルソナ<br>
+				美容とファッションに興味のある女子高生</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">学校カラーを使用しブランディングをイメージして作成しました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">1:30〜2:00</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+	
   <section id="bn_4">
       <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_4.jpg" alt=""></div>
       <h3 class="works_bn_h"><span>札幌ベルエポック製菓調理ウェディング専門学校様</span><br>
-        広告バナー</h3>
+        11月のオープンキャンパス 広告バナー</h3>
       <ul class="accordion-area">
         <li>
           <div class="bn_accordion"><a>
@@ -54,7 +121,7 @@
             <h3 class="works_hp_h">制作ポイント</h3>
             <p class="works_p">北海道は冬になると外出してオープンキャンパスに参加する方が減少するため"雪が降る前に"を記載。
 			  同時に往復送迎があることをアピール。<br>
-			  全体的に秋っぽい雰囲気で"食欲"をそそるような、高校生らしいポップな秋の雰囲気に仕上げました。</p>
+			  全体的に秋らしい雰囲気で"食欲"をそそるような、高校生らしいポップな秋の雰囲気に仕上げました。</p>
             <h3 class="works_hp_h">ツール</h3>
             <p class="works_p">Illustrator / Photoshop</p>
             <h3 class="works_hp_h">制作時間</h3>
@@ -63,6 +130,270 @@
         </li>
       </ul>
     </section>
+	
+  <section id="bn_6">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_6.jpg" alt=""></div>
+      <h3 class="works_bn_h"><span>札幌ベルエポック製菓調理ウェディング専門学校様</span><br>
+        春のオープンキャンパス 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				男女　17〜18才<br>
+				○ペルソナ<br>
+				飲食に興味のある高校生</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">春らしいカラーと、コラボレーションの箇所を別枠で設けることで情報をわかりやすくしました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">2:00</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+  <section id="bn_5">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_5.jpg" alt=""></div>
+      <h3 class="works_bn_h"><span>札幌ベルエポック製菓調理ウェディング専門学校様</span><br>
+        夏のオープンキャンパス 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				男女　17〜18才<br>
+				○ペルソナ<br>
+				飲食に興味のある高校生</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">夏のオープンキャンパスなのでビタミンカラーでまとめました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">2:30</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+	
+  <section id="bn_10">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_10.jpg" alt=""></div>
+      <h3 class="works_bn_h"><span>札幌ベルエポック製菓調理ウェディング専門学校様</span><br>
+        レストランイベント 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				男女　17〜18才<br>
+				○ペルソナ<br>
+				飲食に興味のある高校生</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">レストランがコンセプトのイベントなので高級感を出しました。<br>
+			 また、高校生がターゲットの為全体的に明るくポップに仕上げました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">2:30</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+	
+  <section id="bn_14">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_14.jpg" alt="">
+	  </div>
+      <h3 class="works_bn_h"><span>札幌ベルエポック製菓調理ウェディング専門学校様</span><br>
+        通年使用のオープンキャンパス 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				男女　17〜18才<br>
+			  ○ペルソナ<br>
+				飲食に興味のある高校生</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">楽しい雰囲気を出したかったのでビビットで明るいイメージに仕上げました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">2:30</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+	
+  <section id="bn_15">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_15.jpg" alt="">
+	  </div>
+      <h3 class="works_bn_h"><span>札幌ベルエポック製菓調理ウェディング専門学校様</span><br>
+        ハロウィンと企業のコラボレーションのイベント 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				男女　17〜18才<br>
+			  ○ペルソナ<br>
+				飲食に興味のある高校生</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">ハロウィンと企業のコラボレーションのイベントの為２つにエリアを分けて情報をまとめました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">2:30</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+
+  <section id="bn_8">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_8.jpg" alt=""></div>
+      <h3 class="works_bn_h"><span>札幌ベルエポック製菓調理ウェディング専門学校様</span><br>
+        8校合同学園祭 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				男女　17〜18才</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">学園祭なのでコミカルで楽しい雰囲気に仕上げました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">2:00</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+	
+  <section id="bn_16">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_16.jpg" alt=""></div>
+      <h3 class="works_bn_h"><span>札幌ベルエポック製菓調理ウェディング専門学校様</span><br>
+        保護者会 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				男女　30〜40才</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">保護者向けなので色味を落ち着いたトーンでまとめました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">1:00</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+	
+	
+  <section id="bn_7">
+      <div class="works_bn_main_img">
+        <ul class="col__wrap--margin">
+          <li class="col--pc-8 col--sp-12"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_7.jpg" alt=""></li>
+          <li class="col--pc-4 col--sp-12"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_7_2.jpg" alt=""></li>
+		  </ul>
+	  </div>
+      <h3 class="works_bn_h"><span>ベルエポック美容専門学校様</span><br>
+        AO入試 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">AO入試エントリー</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				女性　17〜18才<br>
+				○ペルソナ<br>
+				流行やおしゃれに敏感でSNSが好きな女子高生</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">ビビットにまとめ目の引くデザインに仕上げました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">1:30〜2:00</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
+	
+	
+  <section id="bn_12">
+      <div class="works_bn_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/banner/bn_12.jpg" alt=""></div>
+      <h3 class="works_bn_h"><span>美作市スポーツ医療看護専門学校</span><br>
+        女性向け 広告バナー</h3>
+      <ul class="accordion-area">
+        <li>
+          <div class="bn_accordion"><a>
+            <h3 class="bn_accordion_ttl">詳細を見る</h3>
+            </a></div>
+          <div class="bn_accordion_box">
+            <h3 class="works_hp_h">目的</h3>
+            <p class="works_p">オープンキャンパスへの集客</p>
+            <h3 class="works_hp_h">ターゲット層</h3>
+            <p class="works_p">○性別・年齢<br>
+				女性　17〜18才</p>
+            <h3 class="works_hp_h">制作ポイント</h3>
+            <p class="works_p">スポーツ分野に女性をターゲットとした広告の為、全体的にピンクでまとめました。</p>
+            <h3 class="works_hp_h">ツール</h3>
+            <p class="works_p">Illustrator / Photoshop</p>
+            <h3 class="works_hp_h">制作時間</h3>
+            <p class="works_p">2:00</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+	
 	
 </div><!--works_bn_wrap-->
 </section><!--content-1000wrap-->
