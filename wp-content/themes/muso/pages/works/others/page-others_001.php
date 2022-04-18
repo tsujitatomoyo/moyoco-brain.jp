24f3b8269189bfe0a9cbdff80f7e496e2157e5a3
辻田智代 <mshxtom@gmail.com>
2022-04-19 08:43:40 +0900

    no message

---
+++ wp-content/themes/muso/pages/works/others/page-others_001.php
@@ -0,0 +1,101 @@
+<section class="PB--30">
+	
+<div class="content-sp">
+<div class="works_hp_main_img"><img src="<?php echo get_template_directory_uri(); ?>/img/works/others/others_001_main.png" alt=""></div>
+<h2 class="works_hp_ttl">札幌ベルエポック製菓調理ウェディング様　<br class="Box-sp">コンテンツ企画・カスタム投稿</h2>
+<p class="works_hp_ttl_sb">コンテンツ企画・カスタム投稿</p>
+
+<div class="works_hp_wrap">
+<table>
+	<tbody>
+		<tr>
+			<td><p class="works_p">担当：</p></td>
+			<td><p class="works_p">提案・カスタム投稿作成・デザイン・コーディング</p></td>
+		</tr>
+		<?php /*?><tr>
+			<td><p class="works_p">言語：</p></td>
+			<td><p class="works_p">HTML/CSS/JavaScript/jQuery</p></td>
+		</tr><?php */?>
+		<tr>
+			<td><p class="works_p">利用：</p></td>
+			<td><p class="works_p">WordPress/Dreamweaver</td>
+		</tr>
+		<tr>
+			<td><p class="works_p">期間：</p></td>
+			<td><p class="works_p">2ヶ月</p></td>
+		</tr>
+		<tr>
+			<td><p class="works_p">URL：</p></td>
+			<td><p class="works_p"><a target="_blank" href="https://www.foods.belle.ac.jp/shushoku/naitei">https://www.foods.belle.ac.jp/shushoku/naitei</a></p></td>
+		</tr>
+		<tr>
+			<td><p class="works_p"></p></td>
+			<td><p class="works_p"><p class="works_p MT--0"><a target="_blank" href="https://www.foods.belle.ac.jp/shushoku/obvoice">https://www.foods.belle.ac.jp/shushoku/obvoice</a></p></p></td>
+		</tr>
+	</tbody>
+</table>
+	
+<h3 class="works_hp_h">概要</h3>
+<p class="works_p">内定者の一覧を毎年掲載しておりましたが、<br>
+	生徒の掲載にあたり削除依頼や修正依頼が多発するコンテンツでした。<br>
+そこでカスタム投稿でシステム化することで先方自身での更新を可能に。<br>
+	さらにこれまで校内での掲載用に先方でPowerPointで自作していたものを、<br>
+内定者ページを印刷するだけで掲載可能へ。</p>
+<h3 class="works_hp_h">目的</h3>
+<p class="works_p">双方の作業効率化</p>
+
+
+</div>
+
+	
+
+
+	
+	<div class="works_hp_wrap">
+		<h3 class="works_hp_h">制作</h3>
+		<p class="works_p">WordPressのカスタム投稿を使用し、内定者ページへの追加を簡易化。<br>
+のちに卒業生のインタビューや地元出身校別の紹介というようなコンテンツへ発展できるように設定しました。<br>
+追加していくごとに数多くの就職先が掲載され学校の強みとなるコンテンツへ。</p>
+ <ul class="col__wrap--margin M--auto">
+      <li class="col--pc-6 col--sp-6"><a class="js-modal-open btn btn-flat" data-target="modal01"><span><i class="fa-solid fa-desktop"></i></span></a></li>
+      <li class="col--pc-6 col--sp-6"><a class="js-modal-open btn btn-flat" data-target="modal02"><span><i class="fas fa-mobile-alt"></i></span></a></li>
+	 <li class="col--pc-6 col--sp-6"><a class="js-modal-open btn btn-flat" data-target="modal03"><span><i class="fa-solid fa-print"></i></span></a></li>
+    </ul>
+    </div>
+</div>
+	
+	
+<div class="works_modal_wrap">
+    <div id="modal01" class="modal js-modal">
+        <div class="modal__bg js-modal-close"></div>
+        <div class="modal__content">
+			<p class="modal__batu"><a class="js-modal-close" href=""><i class="fas fa-times"></i></a></p>
+            <img class="w--100" src="<?php echo get_template_directory_uri(); ?>/img/works/others/others_001_pc.jpg">
+            <p class="Text-Center"><a class="js-modal-close btn--orange" href="">閉じる</a></p>
+        </div><!--modal__inner-->
+    </div><!--modal-->
+
+
+    <div id="modal02" class="modal js-modal">
+        <div class="modal__bg js-modal-close"></div>
+        <div class="modal__content">
+			<p class="modal__batu"><a class="js-modal-close" href=""><i class="fas fa-times"></i></a></p>
+            <img class="w--100" src="<?php echo get_template_directory_uri(); ?>/img/works/others/others_001_sp.jpg">
+            <p class="Text-Center"><a class="js-modal-close btn--orange" href="">閉じる</a></p>
+        </div><!--modal__inner-->
+    </div><!--modal-->
+	
+
+    <div id="modal03" class="modal js-modal">
+        <div class="modal__bg js-modal-close"></div>
+        <div class="modal__content">
+			<p class="modal__batu"><a class="js-modal-close" href=""><i class="fas fa-times"></i></a></p>
+            <img class="w--100" src="<?php echo get_template_directory_uri(); ?>/img/works/others/others_003.jpg">
+            <p class="Text-Center"><a class="js-modal-close btn--orange" href="">閉じる</a></p>
+        </div><!--modal__inner-->
+    </div><!--modal-->
+</div>
+</section>	
+	
+
+
